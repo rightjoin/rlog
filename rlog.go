552cadac61e6e86ea774344af527fecf121a2de4
Harsh K <hkundra@gmail.com>
2019-03-22 16:03:38 +0530

    default implementation

---
+++ rlog.go
@@ -0,0 +1,25 @@
+package rlog
+
+import (
+	"github.com/inconshreveable/log15"
+)
+
+func Debug(msg string, ctx ...interface{}) {
+	log15.Debug(msg, ctx...)
+}
+
+func Info(msg string, ctx ...interface{}) {
+	log15.Info(msg, ctx...)
+}
+
+func Warn(msg string, ctx ...interface{}) {
+	log15.Warn(msg, ctx...)
+}
+
+func Error(msg string, ctx ...interface{}) {
+	log15.Error(msg, ctx...)
+}
+
+func Crit(msg string, ctx ...interface{}) {
+	log15.Crit(msg, ctx...)
+}
