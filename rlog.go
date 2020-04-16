4ce2368867a1b778a1a35294e3e1b40dcaac605f
Harsh K <hkundra@gmail.com>
2020-04-16 21:54:52 +0530

    multi file handling

--- rlog.go
+++ rlog.go
@@ -8,16 +8,17 @@ import (
 	"github.com/rightjoin/fig"
 )
 
-// Context defines any global values
-// that should be added to all logged statements
-var Context = []interface{}{}
-
 // log.format:
 //      terminal
 //		json        (automatically logs to file)
 //		json-pretty (automatically logs to file)
 //      line        (automatically logs to file)
-// log.folder: directory where to store logs
+// log.folder: [./logs] (directory to store log files)
+// log.filename.default: [app]
+// log.filename.key:     [ctx]
+// log.filename.separate:
+//    - access (ctx=access)
+//    - module (ctx=module)
 func init() {
 
 	if "terminal" == fig.StringOr("terminal", "log.format") {
@@ -30,49 +31,39 @@ func init() {
 		panic(err)
 	}
 
-	// Filename
-	appPath := fmt.Sprintf("%s/app.log", dir)
-	//debugPath := fmt.Sprintf("%s/debug.log", dir)
-
-	//yr, m, d := time.Now().Date()
-	//fileName := fmt.Sprintf("%d-%d-%d.log", yr, m, d)
-
-	// Initializes a file-handler for writing all the logs to
-	// the mentioned file
-	fileHandler, err := log15.FileHandler(appPath, getLogFmt())
+	// Default Filename
+	defaultFile := fmt.Sprintf("%s/%s.log", dir, fig.StringOr("app", "log.filename.default"))
+	defaultHandler, err := log15.FileHandler(defaultFile, getLogFormat())
 	if err != nil {
 		panic(err)
 	}
 
-	/*
-		// Initializes a specific file-handler for all the "debug" logs
-		debugFileHandler, err := log15.FileHandler(debugPath, getLogFmt())
+	// Key
+	key := fig.StringOr("ctx", "log.filename.key")
+
+	// Separate Filenames
+	separateFiles := fig.StringSliceOr([]string{}, "log.filename.separate")
+	separateHandlers := make([]log15.Handler, len(separateFiles))
+	for i, f := range separateFiles {
+		fh, err := log15.FileHandler(fmt.Sprintf("%s/%s.log", dir, f), getLogFormat())
 		if err != nil {
 			panic(err)
 		}
+		separateHandlers[i] = log15.MatchFilterHandler(key, f, fh)
+	}
 
-		// Creates a filter-handler that does the filtering of logs based
-		// on its level (in our case every log-level except "debug"),
-		// routing all the filtered logs to the mentioned file-handler
-		aHandler := log15.LvlFilterHandler(log15.LvlInfo, fileHandler)
+	vals := make([]interface{}, len(separateFiles))
+	allHandlers := make([]log15.Handler, len(separateFiles)+1)
+	for i, f := range separateFiles {
+		vals[i] = f
+		allHandlers[i] = separateHandlers[i]
+	}
+	allHandlers[len(separateFiles)] = skipHandler(key, vals, defaultHandler)
 
-		// Defines a specific filter-handler that would only get triggered
-		// for "debug" logs, hence writing all the "debug" logs to the
-		// mentioned file
-		dHandler := log15.FilterHandler(func(r *log15.Record) bool {
-			if r.Lvl == log15.LvlDebug {
-				return true
-			}
-			return false
-		}, debugFileHandler)
-
-		multiHandler := log15.MultiHandler(aHandler, dHandler)
-	*/
-
-	log15.Root().SetHandler(fileHandler)
+	log15.Root().SetHandler(log15.MultiHandler(allHandlers...))
 }
 
-func getLogFmt() log15.Format {
+func getLogFormat() log15.Format {
 	f := fig.StringOr("json", "log.format")
 
 	switch f {
@@ -83,6 +74,21 @@ func getLogFmt() log15.Format {
 	default:
 		return log15.JsonFormat()
 	}
+}
+
+func skipHandler(key string, values []interface{}, h log15.Handler) log15.Handler {
+	return log15.FilterHandler(func(r *log15.Record) (pass bool) {
+		for i := 0; i < len(r.Ctx); i += 2 {
+			if r.Ctx[i] == key {
+				for j, _ := range values {
+					if r.Ctx[i+1] == values[j] {
+						return false
+					}
+				}
+			}
+		}
+		return true
+	}, h)
 }
 
 func Debug(msg string, ctx ...interface{}) {
