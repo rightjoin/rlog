358f56875a338cda9e56fca0826c5b673dede892
Harsh K <hkundra@gmail.com>
2020-04-16 17:43:45 +0530

    simplify

--- rlog.go
+++ rlog.go
@@ -12,75 +12,74 @@ import (
 // that should be added to all logged statements
 var Context = []interface{}{}
 
-// Initialization of the logger is done.
-// Supports following Configurations:
-// 		service.name: catalog-svc (name of the service: MUST)
-// 		log.active: will not log to file if set to false. (default: true)
-//		log.format: json/terminal (format in which the logs will be written; default: json)
-//		log.folder: directory for where to store the logs
+// log.format:
+//      terminal
+//		json        (automatically logs to file)
+//		json-pretty (automatically logs to file)
+//      line        (automatically logs to file)
+// log.folder: directory where to store logs
 func init() {
 
-	shouldLogToFile := fig.BoolOr(true, "log.active")
-	if !shouldLogToFile {
+	if "terminal" == fig.StringOr("terminal", "log.format") {
 		return
 	}
 
-	// Prepare the relevant directory structure
+	// Directory
 	dir := fig.StringOr("./logs", "log.folder")
-
-	svcName := fig.StringOr("general", "service.name")
-	dir += fmt.Sprintf("/%s", svcName)
-
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		panic(err)
 	}
 
+	// Filename
+	appPath := fmt.Sprintf("%s/app.log", dir)
+	//debugPath := fmt.Sprintf("%s/debug.log", dir)
+
 	//yr, m, d := time.Now().Date()
 	//fileName := fmt.Sprintf("%d-%d-%d.log", yr, m, d)
 
-	aPath := fmt.Sprintf("%s/app.log", dir)
-	dPath := fmt.Sprintf("%s/debug.log", dir)
-
 	// Initializes a file-handler for writing all the logs to
 	// the mentioned file
-	fileHandler, err := log15.FileHandler(aPath, getLogFmt())
+	fileHandler, err := log15.FileHandler(appPath, getLogFmt())
 	if err != nil {
 		panic(err)
 	}
 
-	// Initializes a specific file-handler for all the "debug" logs
-	debugFileHandler, err := log15.FileHandler(dPath, getLogFmt())
-	if err != nil {
-		panic(err)
-	}
-
-	// Creates a filter-handler that does the filtering of logs based
-	// on its level (in our case every log-level except "debug"),
-	// routing all the filtered logs to the mentioned file-handler
-	aHandler := log15.LvlFilterHandler(log15.LvlInfo, fileHandler)
-
-	// Defines a specific filter-handler that would only get triggered
-	// for "debug" logs, hence writing all the "debug" logs to the
-	// mentioned file
-	dHandler := log15.FilterHandler(func(r *log15.Record) bool {
-		if r.Lvl == log15.LvlDebug {
-			return true
+	/*
+		// Initializes a specific file-handler for all the "debug" logs
+		debugFileHandler, err := log15.FileHandler(debugPath, getLogFmt())
+		if err != nil {
+			panic(err)
 		}
-		return false
-	}, debugFileHandler)
 
-	multiHandler := log15.MultiHandler(aHandler, dHandler)
+		// Creates a filter-handler that does the filtering of logs based
+		// on its level (in our case every log-level except "debug"),
+		// routing all the filtered logs to the mentioned file-handler
+		aHandler := log15.LvlFilterHandler(log15.LvlInfo, fileHandler)
 
-	log15.Root().SetHandler(multiHandler)
+		// Defines a specific filter-handler that would only get triggered
+		// for "debug" logs, hence writing all the "debug" logs to the
+		// mentioned file
+		dHandler := log15.FilterHandler(func(r *log15.Record) bool {
+			if r.Lvl == log15.LvlDebug {
+				return true
+			}
+			return false
+		}, debugFileHandler)
+
+		multiHandler := log15.MultiHandler(aHandler, dHandler)
+	*/
+
+	log15.Root().SetHandler(fileHandler)
 }
 
-// getLogFmt returns the desired format as per the mentioned config.
 func getLogFmt() log15.Format {
 	f := fig.StringOr("json", "log.format")
 
 	switch f {
-	case "terminal":
-		return log15.TerminalFormat()
+	case "line":
+		return log15.LogfmtFormat()
+	case "json-pretty":
+		return log15.JsonFormatEx(true, true)
 	default:
 		return log15.JsonFormat()
 	}
